5e8b0f7927ae6d4b79410f0ed5e77188169f18b2
Remon Lam <remon.lam@lamnet.nl>
2016-11-11 22:15:21 +0100

    update download function

--- functions/functionDownloadSources.sh
+++ functions/functionDownloadSources.sh
@@ -2,4 +2,9 @@
 
 #########################################################################################
 ### NOTE: This function will download all the required files.
-function functionFormatDisk {
+#########################################################################################
+
+function functionDownloadSources {
+#curl -sSL -o $rootfs/CentOS-7-aarch64-rootfs-1606.tar.xz http://mirror.centos.org/altarch/7/isos/aarch64/CentOS-7-aarch64-rootfs-1606.tar.xz
+wget  -P $rootfs http://mirror.centos.org/altarch/7/isos/aarch64/CentOS-7-aarch64-rootfs-1606.tar.xz
+}
