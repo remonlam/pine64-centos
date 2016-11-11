69795300f089c2ed027113f926cb0ac543cc4dd6
Remon Lam <remon.lam@lamnet.nl>
2016-11-11 22:56:31 +0100

    enabled extract root

--- functions/functionExtractImage.sh
+++ functions/functionExtractImage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function functionExtractImage {
-#tar --numeric-owner -xpJf $rootfs/CentOS-7-aarch64-rootfs-1606.tar.xz -C $rootfs/root
+tar --numeric-owner -xpJf $rootfs/CentOS-7-aarch64-rootfs-1606.tar.xz -C $rootfs/root
 tar -zxvf sources/modules.tar.gz -C $rootfs/root/lib/
 tar -zxvf sources/firmware.tar.gz -C $rootfs/root/lib/
 }
