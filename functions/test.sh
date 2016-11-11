348cb1ecfecb6df06430918c7d8f9ea34998628e
Remon Lam <remon.lam@lamnet.nl>
2016-11-11 22:27:02 +0100

    test

--- functions/test.sh
+++ functions/test.sh
@@ -4,7 +4,6 @@
 ### NOTE: This function will download all the required files.
 #########################################################################################
 
-function functionDownloadSources {
-#curl -sSL -o $rootfs/CentOS-7-aarch64-rootfs-1606.tar.xz http://mirror.centos.org/altarch/7/isos/aarch64/CentOS-7-aarch64-rootfs-1606.tar.xz
-wget  -P $rootfs http://mirror.centos.org/altarch/7/isos/aarch64/CentOS-7-aarch64-rootfs-1606.tar.xz
+function test {
+ls
 }
