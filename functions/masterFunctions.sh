27f76a0f44b8d1dde341fa8d32bc3b1084840c0b
Remon Lam <remon.lam@lamnet.nl>
2016-11-11 21:31:56 +0100

    add new functions

---
+++ functions/masterFunctions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+## Master function script
+## this script will call all functions in this directory
+## Network functions will be loaded in "../functions/functionsNetworkProfileSelection"
+
+## This is just a master test script it wont be used for something :-)
+. ./functions/masterVariables.sh
+
+# Import other functions
+. ./functions/functionFormatDisk.sh
+. ./functions/functionRootCheck.sh
---
+++ functions/masterVariables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+# Set fixed variables, need to move this part to function
+part1=1
+part2=2
+
+# Set fixed variables for mounting sd card
+mount_boot=/tmp/boot
+mount_root=/tmp/root
+
+# Set build directory
+rootfs=/tmp/centos7_aarch64_build
