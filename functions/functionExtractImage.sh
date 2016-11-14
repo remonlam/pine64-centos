60b66f3eda8064d9a91834977626a84618afeadb
Remon Lam <remon.lam@lamnet.nl>
2016-11-14 13:07:55 +0100

    fix typo

--- functions/functionExtractImage.sh
+++ functions/functionExtractImage.sh
@@ -6,6 +6,6 @@ tar -zxvf sources/modules.tar.gz -C $rootfs/root/lib/
 tar -zxvf sources/firmware.tar.gz -C $rootfs/root/lib/
 tar -zxvf sources/boot.tar.gz -C $rootfs/boot/
 # Clean up /root/boot because we're using a different boot mount;
-rm -tf $rootfs/root/boot/
+rm -rf $rootfs/root/boot/
 
 }
