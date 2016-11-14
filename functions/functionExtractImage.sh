45a9718959dae4f470a5391cc7ec8cbffa6466c0
Remon Lam <remon.lam@lamnet.nl>
2016-11-14 11:52:42 +0100

    change to new boot

--- functions/functionExtractImage.sh
+++ functions/functionExtractImage.sh
@@ -4,6 +4,6 @@ function functionExtractImage {
 tar --numeric-owner -xpJf $rootfs/CentOS-7-aarch64-rootfs-1606.tar.xz -C $rootfs/root
 tar -zxvf sources/modules.tar.gz -C $rootfs/root/lib/
 tar -zxvf sources/firmware.tar.gz -C $rootfs/root/lib/
-tar -zxvf sources/pine64_boot.tar.gz -C $rootfs/boot/
+tar -zxvf sources/boot.tar.gz -C $rootfs/boot/
 
 }
